d9f8c6672d6e8815d3f22f50e73403a9230264e3
Gilson Alencar <gilson.alencar94@gmail.com>
2021-11-16 11:10:10 -0300

    feat: add navigation links to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -34,6 +35,12 @@ export default class Header extends Component {
         ) : (
           <h1 data-testid="header-user-name">{userName}</h1>
         )}
+
+        <nav>
+          <Link to="/search" data-testid="link-to-search">Pesquisa</Link>
+          <Link to="/favorites" data-testid="link-to-favorites">Favoritas</Link>
+          <Link to="/profile" data-testid="link-to-profile">Perfil</Link>
+        </nav>
       </header>
     );
   }
