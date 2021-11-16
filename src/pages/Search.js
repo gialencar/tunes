e5d6412998e67bb31f121229a113b38ca7f3ae20
Gilson Alencar <gilson.alencar94@gmail.com>
2021-11-16 16:04:51 -0300

    feat: add search form

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,47 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchQuery: '',
+      searchBtnDisabled: true,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+      searchBtnDisabled: value.length < 2,
+    });
+  }
+
   render() {
+    const { searchQuery, searchBtnDisabled } = this.state;
     return (
       <>
         <Header />
-        <div data-testid="page-search">{/* TODO: */}</div>
+        <div data-testid="page-search">
+          <label htmlFor="query">
+            <input
+              type="text"
+              value={ searchQuery }
+              id="query"
+              name="searchQuery"
+              onChange={ this.handleChange }
+              data-testid="search-artist-input"
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ searchBtnDisabled }
+          >
+            Pesquisar
+          </button>
+        </div>
       </>
     );
   }
