f636413646fecedfe8667173f507e04cc9811d98
Gilson Alencar <gilson.alencar94@gmail.com>
2021-11-18 10:04:58 -0300

    feat(req6): add request to api on search page and album display

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 export default class Search extends Component {
   constructor() {
@@ -8,6 +11,9 @@ export default class Search extends Component {
     this.state = {
       searchQuery: '',
       searchBtnDisabled: true,
+      loading: false,
+      albums: [],
+      result: '',
     };
   }
 
@@ -17,32 +23,79 @@ export default class Search extends Component {
       [name]: value,
       searchBtnDisabled: value.length < 2,
     });
-  }
+  };
+
+  search = async () => {
+    const { searchQuery } = this.state;
+
+    this.setState({ searchQuery: '', loading: true }, async () => {
+      const response = await searchAlbumsAPI(searchQuery);
+      // console.log(response);
+      const result = `Resultado de álbuns de: ${searchQuery}`;
+      this.setState({ albums: response, loading: false, result });
+    });
+  };
 
   render() {
-    const { searchQuery, searchBtnDisabled } = this.state;
+    const {
+      searchQuery,
+      searchBtnDisabled,
+      loading,
+      albums,
+      result,
+    } = this.state;
+
     return (
       <>
         <Header />
-        <div data-testid="page-search">
-          <label htmlFor="query">
-            <input
-              type="text"
-              value={ searchQuery }
-              id="query"
-              name="searchQuery"
-              onChange={ this.handleChange }
-              data-testid="search-artist-input"
-            />
-          </label>
-          <button
-            type="button"
-            data-testid="search-artist-button"
-            disabled={ searchBtnDisabled }
-          >
-            Pesquisar
-          </button>
-        </div>
+        {loading ? (
+          <Loading />
+        ) : (
+          <div data-testid="page-search">
+            <label htmlFor="query">
+              <input
+                type="text"
+                value={ searchQuery }
+                id="query"
+                name="searchQuery"
+                onChange={ this.handleChange }
+                data-testid="search-artist-input"
+              />
+            </label>
+            <button
+              type="button"
+              data-testid="search-artist-button"
+              disabled={ searchBtnDisabled }
+              onClick={ this.search }
+            >
+              Pesquisar
+            </button>
+
+            {albums.length ? (
+              <div>
+                <h3>{result}</h3>
+                {albums.map((album) => (
+                  <div key={ album.collectionId }>
+                    {/* TODO: refactor into separate component */}
+                    <Link
+                      to={ `/album/${album.collectionId}` }
+                      data-testid={ `link-to-album-${album.collectionId}` }
+                    >
+                      <img
+                        src={ album.artworkUrl100 }
+                        alt={ album.collectionName }
+                      />
+                      <h4>{album.collectionName}</h4>
+                      <p>{album.artistName}</p>
+                    </Link>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <h3>Nenhum álbum foi encontrado</h3>
+            )}
+          </div>
+        )}
       </>
     );
   }
