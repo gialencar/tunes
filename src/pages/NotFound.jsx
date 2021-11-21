86c3856871c5de8438a8f844ae2f05cb2c47137d
Gilson Alencar <gilson.alencar94@gmail.com>
2021-11-21 00:16:44 -0300

    commit to re-run evaluator

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,6 +2,6 @@ import React, { Component } from 'react';
 
 export default class NotFound extends Component {
   render() {
-    return <div data-testid="page-not-found">{/* TODO: */}</div>;
+    return <div data-testid="page-not-found">{/* TODO: a */}</div>;
   }
 }
